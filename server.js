ea4303588197b97b84779646300ec5a07d951d0b
kylegould36 <137902630+kylegould36@users.noreply.github.com>
2023-10-12 11:19:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = require("./routes/TodoRoute");
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 3001;
 
 app.use(express.json());
 app.use(cors());
